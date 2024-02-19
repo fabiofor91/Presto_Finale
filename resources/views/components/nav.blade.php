4b83c42bca9c76f76bd68e22eec0d66cf03bb257
Armando Campanella <armandocampanella1992@gmail.com>
2024-02-19 17:29:40 +0100

    carusel e card impostate

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -5,7 +5,7 @@
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="collapse navbar-collapse" id="navbarNav">
         <div class="navbar-nav d-flex mx-auto">
           <a class="nav-link active mx-5" aria-current="page" href="{{route('welcome')}}">Home</a>
           {{-- <a class="nav-link" href="#">Features</a> --}}

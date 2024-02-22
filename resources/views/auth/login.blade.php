6b2431bd169020eb5cfa71b8cffbfdebc025a80f
fabiofor91 <fabiofor91@gmail.com>
2024-02-22 14:39:29 +0100

    aggiunti messaggi validazione per pagina login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,11 +9,17 @@
                 @csrf
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Indirizzo mail</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="mario@rossi.it" name="email">
+                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="exampleInputEmail1" placeholder="mario@rossi.it" name="email">
+                    @error('email')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <label for="exampleInputPassword1" class="form-label @error('password') is-invalid @enderror">Password</label>
                     <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+                    @error('password')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
                 </div>
                 <button type="submit" class="btn btn-primary">Accedi</button>
             </form>
